94a1bc9b3e53b26c53f2754407554a2e54b77e27
Caio Roberto <caio.roberto@sensedia.com>
2022-01-04 17:03:45 -0300

    Warm Up: AnyMatch

--- src/streams/AnyMatch.java
+++ src/streams/AnyMatch.java
@@ -6,15 +6,17 @@ import streams.model.Status;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
+import static streams.model.Status.ACTIVE;
+
 public class AnyMatch {
     public static void main(String[] args) {
         var products = new ArrayList<Product>();
 
-        products.add(new Product("Água 2L", Status.ACTIVE, new BigDecimal("9.9")));
-        products.add(new Product("Picanha 1Kg", Status.ACTIVE, new BigDecimal("109.5")));
+        products.add(new Product("Água 2L", ACTIVE, new BigDecimal("9.9")));
+        products.add(new Product("Picanha 1Kg", ACTIVE, new BigDecimal("109.5")));
         products.add(new Product("Carvão", Status.INACTIVE, new BigDecimal("34.2")));
-        products.add(new Product("Cerveja 600 ml", Status.ACTIVE, new BigDecimal("8.4")));
-        products.add(new Product("Cupim 2Kg", Status.ACTIVE, new BigDecimal("92")));
+        products.add(new Product("Cerveja 600 ml", ACTIVE, new BigDecimal("8.4")));
+        products.add(new Product("Cupim 2Kg", ACTIVE, new BigDecimal("92")));
 
         var doYouHaveSteak = products.stream()
                 .anyMatch(p -> p.getName().equals("Picanha 1Kg"));
